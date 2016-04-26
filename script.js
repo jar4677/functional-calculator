eaf00207aee86e90403ec12ee1d35448b1d61221
Jon Rasmussen <jonathan.a.rasmussen@gmail.com>
2016-04-26 11:49:32 -0700

    calculator v0.5 - Jon Rasmussen

--- script.js
+++ script.js
@@ -1,16 +1,38 @@
 /**
  * Created by jar4677 on 4/25/16.
  */
+
+function callback(type, value, item) {
+
+    switch (value) {
+        case undefined:
+            $('#readout').text("");
+            break;
+        default:
+            $('#readout').text(value);
+            break;
+    }
+}
+
+var myCalculator = new calculator(callback);
+
 $(document).ready(function () {
 
-    function callback(type, value, item) {
-        $("#readout").text(value);
-    }
-
-    myCalculator = new calculator(callback);
-
     $(".button").click(function () {
-        var val = $(this).attr('data-value');
-        myCalculator.addItem(val);
-    })
+
+        var val = $(this).text();
+
+        console.log(val);
+        switch (val){
+            case 'C':
+                myCalculator.clear();
+                break;
+            case 'AC':
+                myCalculator.allClear();
+                break;
+            default:
+                myCalculator.addItem(val);
+        }
+
+    });
 });
